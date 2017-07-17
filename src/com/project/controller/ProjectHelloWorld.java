431aff576829608ecd3c193831bb81fc7ec6f7e9
Asif Hussain <asifbahrian@yahoo.com>
2017-07-17 12:26:14 -0500

    7

--- src/com/project/controller/ProjectHelloWorld.java
+++ src/com/project/controller/ProjectHelloWorld.java
@@ -12,7 +12,7 @@ public class ProjectHelloWorld {
 		String message = "<br><div style='text-align:center;'>"
 				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
 		return new ModelAndView("welcome", "message", message);
-		//change 6
+		//change 7
 	}
 
 }
